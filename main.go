d735abe89f2fabf3706b8a7d27f1fa8affa3742d
enjoykanyu <creatkanyu@outlook.com>
2025-09-14 11:00:06 +0800

    图编排测试

--- main.go
+++ main.go
@@ -1,36 +1,37 @@
 package main
 
 import (
-	"net/http"
+	// "net/http"
 	// agent
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
 	"video_agent/agent"
 )
 
 func main() {
 	// 创建gin引擎
-	r := gin.Default()
+	// r := gin.Default()
 	//运行agent
-	agent.NewAgent()
+	// agent.NewAgent()
+	agent.Graph_agent()
 	// 基本路由
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to Video Agent API with Ollama",
-			"status":  "running",
-			"version": "1.0.0",
-		})
-	})
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "Welcome to Video Agent API with Ollama",
+	// 		"status":  "running",
+	// 		"version": "1.0.0",
+	// 	})
+	// })
 
-	// API路由组
-	api := r.Group("/api")
-	{
-		api.GET("/hello", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"status": "success",
-			})
-		})
-	}
+	// // API路由组
+	// api := r.Group("/api")
+	// {
+	// 	api.GET("/hello", func(c *gin.Context) {
+	// 		c.JSON(http.StatusOK, gin.H{
+	// 			"status": "success",
+	// 		})
+	// 	})
+	// }
 
-	// 启动服务器
-	r.Run(":8080") // 默认在8080端口监听
+	// // 启动服务器
+	// r.Run(":8080") // 默认在8080端口监听
 }
